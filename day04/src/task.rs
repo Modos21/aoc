cb3db6b30708e0b17c8c4a58a5352ca2bf4bbd83
M. Kania <mattes.kania@gmail.com>
2025-12-09 00:15:45 +0100

    started empty implementation

--- day04/src/task.rs
+++ day04/src/task.rs
@@ -1,10 +1,19 @@
 ﻿use framework;
-use framework::Part;
+use framework::Solution;
 
 pub struct Day04;
 
-impl framework::Solution for Day04 {
-    fn run(part: Part) {
-        todo!()
+impl Solution for Day04 {
+    type ParsedInput = String;
+    type ResultType = u64;
+
+    fn part_one(input: Self::ParsedInput) -> Option<u64> {
+        // TODO
+        None
+    }
+
+    fn part_two(input: Self::ParsedInput) -> Option<u64> {
+        // TODO
+        None
     }
 }
